3079a3abc2954d29e98937a7e7b4393c1ca6be83
Administrator <1317660141@qq.com>
2019-10-11 23:53:37 +0800

    存v

--- cloud-demo/consumer-demo/src/main/java/cn/itcast/ConsumerDemoApplication.java
+++ cloud-demo/consumer-demo/src/main/java/cn/itcast/ConsumerDemoApplication.java
@@ -3,6 +3,8 @@ package cn.itcast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConsumerDemoApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- cloud-demo/consumer-demo/src/main/java/cn/itcast/controller/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/cn/itcast/controller/ConsumerController.java
@@ -1,11 +1,13 @@
 package cn.itcast.controller;
 
 import cn.itcast.pojo.User;
+import lombok.val;
 import org.springframework.cloud.client.ServiceInstance;
 import java.util.List;
 import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,14 +27,32 @@ public class ConsumerController {
     @Autowired
     DiscoveryClient discoveryClient;
 
+//    @Autowired
+//    RibbonLoadBalancerClient ribbonLoadBalancerClient;
+
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
-        //String url = "http://127.0.0.1:8081/user/1";
+        //直接访问
+//        String url = "http://127.0.0.1:8081/user/1";
+//        return restTemplate.getForObject(url, User.class);
 
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-services");
-        String url = instances.get(0).getHost() + instances.get(0).getPort() + "/user/" + id;
+        //获取注册中心的服务列表
+//        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+//        String url ="http://" + instances.get(0).getHost() + ":" + instances.get(0).getPort() + "/user/" + id;
+//        return restTemplate.getForObject(url, User.class);
 
+
+        //负载均衡原理  LoadBalancerInterceptor 拦截所有请求
+        //方式1 负载均衡
+//        ServiceInstance serviceInstance = ribbonLoadBalancerClient.choose("user-service");
+//        String url ="http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
+//        return restTemplate.getForObject(url, User.class);
+
+        //方式2 负载均衡  RestTemplate加注解@LoadBalanced
+        String url = "http://user-service/user/1";
         return restTemplate.getForObject(url, User.class);
+
+
     }
 
 }
